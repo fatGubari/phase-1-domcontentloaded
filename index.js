c8fdb989d49a6553c3db38fcb0f0dd12fbf94352
fatima abdullah <fatimajubary@gmail.com>
2023-01-18 20:42:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector( 'p' ).textContent = "This is really cool!"
+  });
+
+  document.querySelector( 'p' ).textContent = "JavaScript is so cool. It lets me add text to my page programmatically."
